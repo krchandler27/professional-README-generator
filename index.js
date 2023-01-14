2f4c401bdda5c28fdbdc0de6e5132dc6b6a80425
krchandler27 <krchandler27@gmail.com>
2023-01-14 11:26:14 -0700

    added a question, got rid of some TODO comments that are now complete, fixed name of function to be consistent with starter-code

--- index.js
+++ index.js
@@ -1,9 +1,7 @@
-// TODO: Include packages needed for this application
 const inquirer = require("inquirer");
 const fs = require("fs");
-const generateMarkDown = require("./utils/generateMarkdown");
+const generateMarkdown = require("./utils/generateMarkdown");
 
-// TODO: Create an array of questions for user input
 const questions = [
     {
         type: "input",
@@ -52,28 +50,31 @@ const questions = [
     },
     {
         type: "input",
+        name: "deployedLink",
+        message: "Enter the link to your deployed project:"
+    },
+    {
+        type: "input",
         name: "email",
         message: "Enter your email address:"
-    },
-
-    
+    }
 ];
 
 // TODO: Create a function to write README file
 function writeToFile(fileName, data) {
-    // fs.writeFile(fileName, generateMarkDown(data))
+    // fs.writeFile(fileName, generateMarkdown(data))
 }
 
 // TODO: Create a function to initialize app
 function init() {
     inquirer
-  .prompt(
-    questions
-  )
-  .then((answers) => {
-    console.log(answers)
-    // writeToFile("README.md", answers)
-  })
+        .prompt(
+            questions
+        )
+        .then((answers) => {
+            console.log(answers)
+            // writeToFile("README.md", answers)
+        })
 }
 
 // Function call to initialize app
